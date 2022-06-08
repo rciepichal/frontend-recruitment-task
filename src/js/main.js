c5219c5f24f633c97db79da67403303e4df8f97e
Radek <r.ciepichal@gmail.com>
2022-06-08 17:32:58 +0200

    bonus-activity-api-connection

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,31 @@ const spanText = document.querySelector('#counterText');
 const modal = document.querySelector('#modalContent');
 const modalAlert = document.querySelector('.section__alert');
 const icon = document.querySelector('#closeIcon');
+const url = 'https://jsonplaceholder.typicode.com/users';
+const usersWrapper = document.querySelector('#usersWrapper');
+const usersTable = document.querySelector('#usersTable');
+const loader = document.querySelector('#loader');
 let count = 0;
 (function () {
   if (!!localStorage.getItem('counterValue')) {
     count = Number(localStorage.getItem('counterValue'));
   }
 })();
+
+const usersService = async () => {
+  try {
+    const response = await fetch(url);
+    if (response.status >= 200 && response.status < 300) {
+      const data = await response.json();
+      hideLoader();
+      generateTable(data);
+    } else {
+      usersWrapper.innerHTML = `<h3>Sorry, we are unable to show data. Try to refresh a page.`;
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 const showModal = () => {
   modalAlert.classList.remove('hide');
@@ -45,6 +64,32 @@ const countReset = () => {
   resetBtnToggler();
 };
 
+const rowStyle = (id) => {
+  if (id % 2 !== 0) {
+    return 'even';
+  }
+};
+
+const generateTable = (users) => {
+  for (const user of users) {
+    const { id, name, email, address, phone, company } = user;
+    usersTable.style.display = 'table';
+    const tRow = document.createElement('tr');
+    tRow.innerHTML = `
+    <td>${name}</td>
+    <td>${email}</td>
+    <td>${address.city}, ${address.street} ${address.suite}</td>
+    <td>${phone}</td>
+    <td>${company.name}</td>`;
+    tRow.classList.add(rowStyle(id));
+    usersTable.append(tRow);
+  }
+};
+
+const hideLoader = () => {
+  loader.style.display = 'none';
+};
+
 showBtn.addEventListener('click', () => {
   counter();
   countTextUpdate();
@@ -62,3 +107,5 @@ window.addEventListener('click', (e) => {
     hideModal();
   }
 });
+
+usersService();
